e357e2def521055e81c17d29294cf8405eee59e6
vijay <anandvijay838@gmail.com>
2025-01-03 20:17:59 +0530

    cust-entity-variables-added

--- spring-boot-hotel-rest/src/main/java/com/hotel/Entity/Customer.java
+++ spring-boot-hotel-rest/src/main/java/com/hotel/Entity/Customer.java
@@ -1,5 +1,18 @@
 package com.hotel.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Customer {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Integer customerId;
+    private String customerName;
+    private Long customerPhone;
     
 }
