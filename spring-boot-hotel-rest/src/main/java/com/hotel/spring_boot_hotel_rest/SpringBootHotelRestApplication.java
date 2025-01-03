9e4fd152d9e2fda22026afb060355c2046333d9e
PUSHKAR D <pushkardwarkanath@gmail.com>
2025-01-03 13:47:53 +0530

    FIRST-COMMIT

---
+++ spring-boot-hotel-rest/src/main/java/com/hotel/spring_boot_hotel_rest/SpringBootHotelRestApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.spring_boot_hotel_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHotelRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHotelRestApplication.class, args);
+	}
+
+}
