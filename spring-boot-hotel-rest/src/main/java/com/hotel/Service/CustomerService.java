b67b24596e58d0c76cdcc3f968df92ca7be1ea63
vijay <anandvijay838@gmail.com>
2025-01-06 22:00:34 +0530

    CustomerService-met-added

--- spring-boot-hotel-rest/src/main/java/com/hotel/Service/CustomerService.java
+++ spring-boot-hotel-rest/src/main/java/com/hotel/Service/CustomerService.java
@@ -1,5 +1,14 @@
 package com.hotel.Service;
 
+import java.util.List;
+
+import com.hotel.Entity.Customer;
+
 public interface CustomerService {
     
+    
+	public Customer saveCustomer (Customer customer);
+	public List<Customer> getAllCustomer();
+    public Customer getCustomerById(Integer id);
+    public String deleteCustomer (Integer id);
 }
