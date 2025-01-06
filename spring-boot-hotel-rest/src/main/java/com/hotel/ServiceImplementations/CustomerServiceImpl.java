c40c6eca1af8744ee064a7808dda02a9bfdb2c6d
vijay <anandvijay838@gmail.com>
2025-01-06 22:10:33 +0530

    CustomerServiceImpl-struct-add

--- spring-boot-hotel-rest/src/main/java/com/hotel/ServiceImplementations/CustomerServiceImpl.java
+++ spring-boot-hotel-rest/src/main/java/com/hotel/ServiceImplementations/CustomerServiceImpl.java
@@ -1,7 +1,34 @@
 package com.hotel.ServiceImplementations;
 
+import java.util.List;
+
+import com.hotel.Entity.Customer;
 import com.hotel.Service.CustomerService;
 
 public class CustomerServiceImpl implements  CustomerService{
+
+    @Override
+    public String deleteCustomer(Integer id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAllCustomer() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Customer getCustomerById(Integer id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Customer saveCustomer(Customer customer) {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
